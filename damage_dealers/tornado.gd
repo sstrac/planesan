67278d831723e606cf15fecd7f6c9ae6f3775204
sstrac <s.r.strachan@outlook.com>
2025-08-16 19:00:35 +0100

    add tornado as objective

--- damage_dealers/tornado.gd
+++ damage_dealers/tornado.gd
@@ -1,15 +1,18 @@
 extends Path2D
 
-
+@export var is_objective: bool = true
 @export var path_start_ratio: float = 0
 @onready var follow: PathFollow2D = get_node("PathFollow2D")
+@onready var obj_manager = get_node("PathFollow2D/ObjectiveManager")
 
 var dir = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	follow.progress_ratio = path_start_ratio
-
+	if is_objective:
+		obj_manager.activate_objective_collision_layer()
+		obj_manager.set_objective()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
